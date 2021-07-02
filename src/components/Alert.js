e22a255e3ed36a9bd1f7ffc6fb2a397198cb7b59
AirMaks <rm1010@yandex.ru>
2021-07-02 10:39:57 +0300

    working with firebase

--- src/components/Alert.js
+++ src/components/Alert.js
@@ -19,4 +19,4 @@ export const Alert = () => {
                 onClick={hide}></button>
         </div>
     )
-}
+} 
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,17 +1,25 @@
-import React, {Fragment} from 'react';
+import React, {Fragment, useContext, useEffect} from 'react';
 import { Form } from '../components/Form';
+import { Loader } from '../components/Loader';
 import { Notes } from '../components/Notes';
+import { FirebaseContext } from '../context/firebase/firebaseContext';
 
 
 export const Home = () => {
-    const notes = new Array(3).fill('').map((_, i) => ({
-        id: i,
-        title: `Note ${+1}`
-    }))
+
+    const {loading, notes, fetchNotes} = useContext(FirebaseContext)
+
+    useEffect(() => {
+        fetchNotes()
+    }, [])
     return (
         <Fragment>
             <Form />
             <hr/>
+            {loading 
+            ? <Loader />
+            : <Notes notes={notes}/>
+            }
             <Notes notes={notes}/>
         </Fragment>
     )
